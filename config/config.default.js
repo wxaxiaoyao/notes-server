12da41f5fef0a2b869327f5cf525f37064a101de
wxaxiaoyao <765485868@qq.com>
2018-11-02 18:55:55 +0800

    xxx

---
+++ config/config.default.js
@@ -0,0 +1,30 @@
+
+exports.keys = "keepwork";
+
+exports.cors = {
+	origin: "*",
+}
+
+exports.middleware = ['authenticated', 'pagination'];
+
+exports.security = {
+	xframe: {
+		enable: false,
+	},
+	csrf: {
+		enable: false,
+	},
+}
+
+exports.onerror = {
+	all: (e, ctx) => {
+		const message = e.stack || e.message || e.toString();
+
+		ctx.status = e.status || 500;
+		ctx.body = message;
+		if (e.name == "SequelizeUniqueConstraintError") {
+			ctx.status = 409;
+		}
+	}
+}
+
