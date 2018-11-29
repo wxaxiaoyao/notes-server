4fa2b5cffa12f3c62779151878d23c30caaba3b6
wxaxiaoyao <765485868@qq.com>
2018-11-29 18:43:07 +0800

    xxx

--- app/controller/code.js
+++ app/controller/code.js
@@ -5,10 +5,10 @@ const Controller = require("../core/controller.js");
 
 const Code = class extends Controller {
 	all() {
-
 		setTimeout(() => {
-			const cmd_str = "git reset --hard HEAD; git pull origin master; npm install; pm2 restart note-server";
+			const cmd_str = "git reset --hard HEAD; git pull origin master; npm install";
 			shell.exec(cmd_str);
+			shell.exec("pm2 restart note-server");
 		}, 100);
 
 		return this.success("OK");
