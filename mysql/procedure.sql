6ccad6757c641979449b4890248209e597e40334
wxaxiaoyao <765485868@qq.com>
2018-12-28 21:13:40 +0800

    xxx

--- mysql/procedure.sql
+++ mysql/procedure.sql
@@ -2,3 +2,11 @@
 
 select * from classifyTags;
 
+delimiter //
+CREATE FUNCTION `lat_lng_distance` (lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	RETURN ROUND(6378.138 * 2 * ASIN(SQRT(POW(SIN((lat1 * PI() / 180 - lat2 * PI() / 180) / 2), 2) + COS(lat1 * PI() / 180) * COS(lat2 * PI() / 180) * POW(SIN(( lon1 * PI() / 180 - lon2 * PI() / 180 ) / 2), 2))),2);
+END //
+delimiter ;
